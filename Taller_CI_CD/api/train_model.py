4d2cc899bce09b23ef544ecc0cf3fb36a95ef6b4
RubenDRC <rubendari98@hotmail.com>
2025-05-20 18:50:27 -0500

    Actualiza train_model.py

--- Taller_CI_CD/api/train_model.py
+++ Taller_CI_CD/api/train_model.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 
 
-# Prueba cambios 12
+# Prueba cambios 13
 
 # Leer el dataset desde la carpeta local
 df = pd.read_csv("data/penguins_size.csv")
